96a4546bdfaa1564a814ce21a711929e9178f3ff
ILLIA Stasiuk <bobicp01@gmail.com>
2025-04-12 10:57:16 +0200

    react_person v3.0

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,17 +1,17 @@
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
 
-  const partners = sex === 'm' ? 'wife' : 'husband';
+  const partnerMass = sex === 'm' ? 'wife' : 'husband';
 
   const isMarrieds = isMarried
-    ? `${partnerName} is my ${partners}`
+    ? `${partnerName} is my ${partnerMass}`
     : 'I am not married';
 
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
       {age && <p className="Person_age">I am {age}</p>}
-      <p className="Person__partner">{isMarrieds} is my wife</p>
+      <p className="Person__partner">{isMarrieds}</p>
     </section>
   );
 };
