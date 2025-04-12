7dab20b2b47175962c1de342e554afdf9de1c4ee
ILLIA Stasiuk <bobicp01@gmail.com>
2025-04-12 10:48:31 +0200

    react_person v2.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import { Person } from './components/Person/Person';
+import { alex, misha, olya, Person } from './components/Person/Person';
 
 export const App = () => (
   <div className="App">
@@ -9,25 +9,3 @@ export const App = () => (
     <Person person={alex} />
   </div>
 );
-
-export const misha = {
-  name: 'Misha',
-  age: 37,
-  sex: 'm',
-  isMarried: true,
-  partnerName: 'Natasha',
-};
-
-export const olya = {
-  name: 'Olya',
-  sex: 'f',
-  isMarried: true,
-  partnerName: 'Maksym',
-};
-
-export const alex = {
-  name: 'Alex',
-  age: 25,
-  sex: 'm',
-  isMarried: false,
-};
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,7 +1,8 @@
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
 
-  const partners = sex === 'm' ? 'Woomen' : 'Man';
+  const partners = sex === 'm' ? 'wife' : 'husband';
+
   const isMarrieds = isMarried
     ? `${partnerName} is my ${partners}`
     : 'I am not married';
@@ -13,4 +14,26 @@ export const Person = ({ person }) => {
       <p className="Person__partner">{isMarrieds} is my wife</p>
     </section>
   );
+};
+
+export const misha = {
+  name: 'Misha',
+  age: 37,
+  sex: 'm',
+  isMarried: true,
+  partnerName: 'Natasha',
+};
+
+export const olya = {
+  name: 'Olya',
+  sex: 'f',
+  isMarried: true,
+  partnerName: 'Maksym',
+};
+
+export const alex = {
+  name: 'Alex',
+  age: 25,
+  sex: 'm',
+  isMarried: false,
 };
